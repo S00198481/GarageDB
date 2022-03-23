d5f8949ea54a240d616834d602f040137061fefd
CianTivnan <cian.tivnan@yahoo.ie>
2022-03-23 13:52:20 +0000

    firebase tools & bootstrap installed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyABlqXesrCagGDCmt0MvLuJpmy2W76VD90",
+    authDomain: "garagedb-a09fe.firebaseapp.com",
+    projectId: "garagedb-a09fe",
+    storageBucket: "garagedb-a09fe.appspot.com",
+    messagingSenderId: "1031172354305",
+    appId: "1:1031172354305:web:c02f572dd582440a15eb00"
+  }
 };
 
 /*
