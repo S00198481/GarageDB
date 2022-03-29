2df3278afb1a756925068ad44ce1f44ce63a609f
CianTivnan <cian.tivnan@yahoo.ie>
2022-03-29 16:08:55 +0100

    routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CarsComponent } from './cars/cars.component';
+import { LoginComponent } from './login/login.component';
+import { CardetailsComponent } from './cardetails/cardetails.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/cars', pathMatch: 'full'},
+  {path: 'cars', component: CarsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'cardetails', component: CardetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
