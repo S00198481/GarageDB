8a0cd26a49dd1fef6f10bb8a87c98ea3cac9880f
CianTivnan <cian.tivnan@yahoo.ie>
2022-04-02 17:03:14 +0100

    verify email added

---
+++ src/app/verify-email/verify-email.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { NgAuthService } from '../services/ng-auth.service';
+
+@Component({
+  selector: 'app-verify-email',
+  templateUrl: './verify-email.component.html',
+  styleUrls: ['./verify-email.component.css']
+})
+export class VerifyEmailComponent implements OnInit {
+
+  constructor(public ngAuthService: NgAuthService) {}
+
+  ngOnInit(): void {}
+
+}
